252752e2703b934a6ba9d5910d4240a27cd1d780
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-10 20:39:41 +0530

    Addd: LastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.*;
 
+
 public class UserRegistration {
     Scanner sc = new Scanner(System.in);
     public void firstNameCheck(){
@@ -25,6 +26,17 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid Last Name with Only \"One Starting Capital\" latter");
     }
 
+    public void email(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Email of User: " );
+        String  eMail = sc.next();
+        boolean check = Pattern.matches("^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*", eMail);
+        if (check)
+            System.out.println("Perfect! #Email!");
+        else
+            System.out.println("Please Enter a Valid Email with Only \"example.abc@bl.co.in\" latter");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         UserRegistration user = new UserRegistration();
@@ -33,6 +45,6 @@ public class UserRegistration {
 
         user.firstNameCheck();
         user.lastNameCheck();
-
+        user.email();
     }
 }
