a9139a6e7a628685ca7643d998d763a8c36e6089
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-10 20:37:17 +0530

    Added: Welcome_Programme

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,7 +2,6 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.*;
 
-
 public class UserRegistration {
     Scanner sc = new Scanner(System.in);
     public void firstNameCheck(){
@@ -15,8 +14,16 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid First name with Only \"One Starting Capital\" latter");
     }
 
-
-
+    public void lastNameCheck(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Last Name of User: " );
+        String  lName = sc.next();
+        boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", lName);
+        if (check)
+            System.out.println("Perfect! Last #Name");
+        else
+            System.out.println("Please Enter a Valid Last Name with Only \"One Starting Capital\" latter");
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -25,8 +32,7 @@ public class UserRegistration {
         System.out.println("Welcome to User Registration Program");
 
         user.firstNameCheck();
-
-
+        user.lastNameCheck();
 
     }
 }
